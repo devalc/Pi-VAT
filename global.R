23bd7f6f08d780ac2619ac5fbf33f6a289be5382
devalc <deva4998@vandals.uidaho.edu>
2021-10-19 16:32:06 -0700

    Update global.R

--- global.R
+++ global.R
@@ -21,6 +21,26 @@ thumbnail_label1 <- function(image, label, content){
 }
 
 
+thumbnail_label2 <- function(image, label, content, helpinfo){
+    div(class = "row", style="height:400px",
+        div(class = "col-sm-14 col-md-12",
+            div(class = "thumbnail",style="height:430px;", #background-color:#6d6875;
+                img(src = image, alt = "...", width ="100%",
+                    div(class = "caption",style="max-width: 100%;text-align:centre",
+                        h4(label)%>%
+                            helper(
+                                icon = "info-circle",
+                                colour = "#fb8500",
+                                content = helpinfo,
+                                type = "markdown",
+                                size = "l",
+                                buttonLabel = "Okay",
+                                easyClose = TRUE,
+                                fade = TRUE
+                            )), div(class = "description",style="max-width:90%;text-justify:inter-word", p(content)
+                        )))))
+}
+
 
 disconnected <- tagList(
     h1("Whoah there..."),
